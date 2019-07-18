85fb7ac2e47ae061e567edff3141371055f0f242
brandondorner <51007432+brandondorner@users.noreply.github.com>
2019-07-18 15:30:06 -0500

    Finished functionality

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect , useState } from 'react';
+
 import './App.css';
 
 function App() {
+
+  const [jokeSetup, setJokeSetup] = useState([]);
+  const [delivery, setDelivery] = useState([]);
+  const [oneliner, setOneliner] = useState([]);
+  const [count, setCount] = useState([])
+
+  const Click = () => {
+    setCount(count + 1)
+  }
+
+  useEffect(() => {
+    
+    const getJoke = async () => {
+      const response = await fetch("https://sv443.net/jokeapi/category/Any")
+      const data = await response.json()
+      console.log(data)
+      setJokeSetup(data.setup)
+      setDelivery(data.delivery)
+      setOneliner(data.joke)
+    }
+
+    getJoke()
+  }, [count] )
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+        <div className="container">
+                <h1>You got jokes??</h1>
+
+            <div className="joke-container">
+                <div id="joke" className="joke">
+                  {jokeSetup ? jokeSetup : null}
+                  <br />
+                  <br />
+                  {delivery ? delivery : null}
+                  {oneliner ? oneliner : null}
+                </div>
+            </div>
+            <button className="generate-btn" onClick={Click} >Generate</button>
+        </div>
+        <script src="app.js"></script>
     </div>
+
   );
 }
 
